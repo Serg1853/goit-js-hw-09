7713e0a713f58d6215b488e18a21d4868814b9b5
Serg1853 <129977140+Serg1853@users.noreply.github.com>
2023-08-27 21:44:02 +0300

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,8 @@
 const button = document.querySelector('.button');
+const date = new Date();
+
+console.log(date);
+// "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+
+console.log(date.toString());
+// "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
