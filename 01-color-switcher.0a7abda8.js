a2cf3f10b101bed1669d2b3bff17962f7f408e80
Serg1853 <129977140+Serg1853@users.noreply.github.com>
2023-08-27 18:45:06 +0000

    Deploying to gh-pages from @ Serg1853/goit-js-hw-09@7713e0a713f58d6215b488e18a21d4868814b9b5 🚀

---
+++ 01-color-switcher.0a7abda8.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".button");var o=new Date;console.log(o),console.log(o.toString())}();
+//# sourceMappingURL=01-color-switcher.0a7abda8.js.map
---
+++ 01-color-switcher.7781b52b.js
@@ -0,0 +1,2 @@
+document.querySelector(".button");const o=new Date;console.log(o),console.log(o.toString());
+//# sourceMappingURL=01-color-switcher.7781b52b.js.map
