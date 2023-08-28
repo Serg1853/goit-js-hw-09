9d7ac403805eff4dd81eb03d03792254b6a9c072
Serg1853 <129977140+Serg1853@users.noreply.github.com>
2023-08-28 18:21:08 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
-const button = document.querySelector('.button');
+const CHANGE_COLOR_DELAY = 1000;
+let idInt = null;
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+refs.startBtn.addEventListener('click', onStartBtn);
+refs.stopBtn.addEventListener('click', onStopBtn);
+
+function onStartBtn() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  idInt = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_COLOR_DELAY);
+}
+
+function onStopBtn() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+  clearInterval(idInt);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
